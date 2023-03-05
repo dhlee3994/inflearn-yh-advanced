08eea7ab3cbe021de37ff97cb0369ad51a0760c9
dhlee3994 <dhlee3994@gmail.com>
2023-03-05 10:36:53 +0900

    Fix: traceId level 감소 부분 수정

--- src/main/java/dev/dhlee/advanced/trace/TraceId.java
+++ src/main/java/dev/dhlee/advanced/trace/TraceId.java
@@ -29,7 +29,7 @@ public class TraceId {
     }
 
     public TraceId createPreviousId() {
-        return new TraceId(id, level + 1);
+        return new TraceId(id, level - 1);
     }
 
     public boolean isFirstLevel() {
